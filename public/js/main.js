e2ce1d41a93e772415361ba06eb756ef62b8d87a
JcarlosCabAlv <jcarloscabalv@gmail.com>
2024-02-18 13:57:17 +0100

    incluido mapa sin datos

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+// variables
+let map = L.map('map').setView([51.505, -0.09], 13);
+let marker = L.marker([51.5, -0.09]).addTo(map);
+
+// functions
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
