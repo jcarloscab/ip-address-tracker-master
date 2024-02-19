7d846765c17cba5d6caa99f2fbd890582490cd0d
JcarlosCabAlv <jcarloscabalv@gmail.com>
2024-02-19 21:57:49 +0100

    ajustes css de board y funcion de recogida de datos y primer mapa

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,34 @@
 // variables
-let map = L.map('map').setView([51.505, -0.09], 13);
-let marker = L.marker([51.5, -0.09]).addTo(map);
+
+const ip = document.getElementById('ip');
+const myLocation = document.getElementById('location');
+const timeZone = document.getElementById('time-zone');
+const isp = document.getElementById('isp');
 
 // functions
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+
+const getData = async (url)=>{
+    let data = await(await (fetch(url))).json();
+    // colocamos los datos en el board
+    let latitud = data.location.lat;
+    let longitud = data.location.lng;
+    ip.textContent = data.ip;
+    myLocation.textContent = `${data.location.city}, ${data.location.region}`;
+    timeZone.textContent = `
+    UTC${data.location.timezone}
+    `
+    isp.textContent = data.isp;
+    
+    // map
+    let map = L.map('map').setView([latitud, longitud], 15);
+    let marker = L.marker([latitud, longitud]).addTo(map);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    // end of map
+}
+
+
+getData('https://geo.ipify.org/api/v2/country,city?apiKey=at_kycNubxWdI4ceeaC944F5VKlUz5XD&ipAddress=');
+// getData('https://geo.ipify.org/api/v2/country,city?apiKey=at_kycNubxWdI4ceeaC944F5VKlUz5XD&domain=');
